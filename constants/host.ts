bc2e0a2a11b34f6f895307bb4736b8c10009b856
Ke Dat <datmartin10@gmail.com>
2024-12-28 07:54:25 +0700

    Update host

--- constants/host.ts
+++ constants/host.ts
@@ -1,3 +1,2 @@
 export const HOST = "https://chatbot-api-g88f.onrender.com/chat";
-export const CUSTOM_HOST =
-  "https://01be-2402-800-61ee-f236-4032-ff70-3d4f-59bd.ngrok-free.app/chat";
+export const CUSTOM_HOST = "https://7128-113-160-170-187.ngrok-free.app/chat";
